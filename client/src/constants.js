dca671e2223bed9fa1aecbf312af0d5b025dbbbb
DiegoTavelli <diegotavelli@gmail.com>
2022-08-12 03:01:31 -0300

    correct path

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://the-pokemon-db.herokuapp.com/"
+export const BASE_URL = "https://the-pokemon-db.herokuapp.com"
 export const POKEMONS_URL = BASE_URL + "/pokemons"
 export const TYPES_URL = BASE_URL + "/types"
