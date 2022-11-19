ba914461d440803f1e545806bf815afe1d17f2ab
DiegoTavelli <diegotavelli@gmail.com>
2022-11-19 05:25:44 -0300

    deploy

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://pokemon-api-db.vercel.app/"
+export const BASE_URL = "https://pokemon-api-db.vercel.app"
 export const POKEMONS_URL = BASE_URL + "/pokemons"
 export const TYPES_URL = BASE_URL + "/types"
 
