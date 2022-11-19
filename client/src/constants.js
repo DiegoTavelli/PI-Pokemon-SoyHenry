7eeec1017ba71632cc0bbe3a3ef703a930a10091
DiegoTavelli <diegotavelli@gmail.com>
2022-11-19 01:04:09 -0300

    prueba back vercel

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,10 @@
-export const BASE_URL = "https://the-pokemon-db.herokuapp.com"
+export const BASE_URL = "https://pokemon-api-db.vercel.app/"
 export const POKEMONS_URL = BASE_URL + "/pokemons"
 export const TYPES_URL = BASE_URL + "/types"
 
+
+//vercel
+//"https://pokemon-api-db.vercel.app/"
 
 //heroku
 // export const BASE_URL = "https://the-pokemon-db.herokuapp.com"
